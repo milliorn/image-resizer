12f63f9535b2980d74374ec1413890924a5e42c7
milliorn <scottmilliorn@gmail.com>
2022-10-04 13:25:50 -0700

    createMainWindow

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello electron");
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+function createMainWindow() {
+  /* instance main browser window */
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 640,
+    height: 360,
+  });
+
+  /* load render/front end */
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
