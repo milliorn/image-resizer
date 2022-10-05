0c4e004cf00147ecef7325f8655b3d1e45206c3f
milliorn <scottmilliorn@gmail.com>
2022-10-04 20:22:18 -0700

    renderer

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,24 +1,30 @@
 // Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
 
-const form = document.querySelector('#img-form');
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const filename = document.querySelector("#filename");
+const heightInput = document.querySelector("#height");
+const widthInput = document.querySelector("#width");
 
 function loadImage(e) {
+  /* get the image */
   const file = e.target.files[0];
 
   if (!isFileImage(file)) {
-      alert('Please select an image file');
-        return;
+    alert("Select an image file");
+    return;
   }
 
-  form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+  form.style.display = "block";
+  /* document.querySelector("#filename") */ filename.innerHTML = file.name;
 }
 
 function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+  const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
+  return file && acceptedImageTypes.includes(file["type"]);
 }
 
-document.querySelector('#img').addEventListener('change', loadImage);
+img.addEventListener("change", loadImage);
+
+document.querySelector("#img").addEventListener("change", loadImage);
