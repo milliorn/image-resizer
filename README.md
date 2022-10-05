f2a51a9de3a6cd827eafdabb0223df224523473d
milliorn <scottmilliorn@gmail.com>
2022-10-04 18:18:10 -0700

    menu

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # desktop-image-resizer
 
-electronjs -> https://www.electronjs.org
+electronjs -> <https://www.electronjs.org>
+
+electronmon -> <https://github.com/catdad/electronmon>
+
+BrowserWindow -> <https://www.electronjs.org/docs/latest/api/browser-window>
+
+Accelerator -> <https://www.electronjs.org/docs/latest/api/accelerator>
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const isDev = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
@@ -10,6 +10,8 @@ function createMainWindow() {
     title: "Image Resizer",
     width: isDev ? 1280 : 640,
     height: 360,
+    x: 0,
+    y: 0,
   });
 
   // open devtools if in dev
@@ -24,11 +26,29 @@ function createMainWindow() {
 app.whenReady().then(() => {
   createMainWindow();
 
+  /* add menu */
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
   // Open a window if none are open (macOS)
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   });
 });
+
+/* menu */
+const menu = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+        accelerator: "CmdOrCtrl+W", // shortcut to Quit
+      },
+    ],
+  },
+];
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
